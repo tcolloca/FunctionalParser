cedfbca7845e5c9852160a9486315d9686e290b7
Tomas Colloca <tomascllc94@gmail.com>
2017-05-18 15:49:27 -0300

    adding chainr, chainl, greedy, compulsion and numbers parser

--- paper1.hs
+++ paper1.hs
@@ -1,4 +1,5 @@
 import Data.Char (ord, isDigit)
+import Data.List
 
 type Parser symbol result = [symbol] -> [([symbol], result)]
 type DetParser symbol result = [symbol] -> result
@@ -6,13 +7,26 @@ type DetParser symbol result = [symbol] -> result
 data Tree = Nil | Bin (Tree,Tree) deriving Show
 
 infixr 6 <.>, <., .>
-infixr 5 <@
+infixr 5 <@, <?@
 infixr 4 <|>
 
 -- Basic Functions --
 
+power :: Fractional a => Int -> a
+power e | e < 0 = 1.0 / power (-e)
+        | otherwise = fromIntegral 10^e
+
 list :: (c, [c]) -> [c]
 list (x, xs) = x:xs
+
+ap :: (a -> b, a) -> b
+ap (f, x) = f x
+
+ap1 :: (a, a -> b -> c) -> b -> c 
+ap1 (a, op) = (a `op`)
+
+ap2 :: (a -> b -> c, b) -> a -> c 
+ap2 (op, y) = (`op` y)
 
 -- Elementary Parsers --
 
@@ -35,6 +49,8 @@ epsilon = succeed ()
 none :: Parser a r
 none s = []
 
+-- Numbers --
+
 digit :: Parser Char Int
 digit = satisfy isDigit <@ f
                where f x = ord x - ord '0'
@@ -42,6 +58,34 @@ digit = satisfy isDigit <@ f
 natural :: Parser Char Int
 natural = many1 digit <@ foldl f 0
           where f a b = a * 10 + b 
+
+integer :: Parser Char Int
+integer = (option (symbol '-') <?@ (id, const negate)) 
+          <.> 
+          natural 
+          <@ ap     
+
+fract :: Parser Char Float
+fract = many1 digit <@ foldr f 0.0
+        where f a b = (b + fromIntegral a) / 10.0
+
+fixed :: Parser Char Float
+fixed = ((option (symbol '-') <?@ (id, const negate)) 
+        <.> 
+        (((option natural <?@ (0, fromIntegral))
+        <.> 
+        (option (symbol '.' .> fract) <?@ (0.0, id))) 
+        <@ uncurry (+))
+        <@ ap)
+        <~>
+        (epsilon <@ const 0)
+
+float :: Parser Char Float
+float = fixed
+        <.>
+        (option (((symbol 'e') <|> (symbol 'E')) .> integer) <?@ (0, id))
+        <@ f
+        where f (m,e) = m * power e         
 
 -- Enclosed --
 
@@ -98,6 +142,9 @@ p1 <:.> p2 = p1 <.> p2 <@ list
 (<|>) :: Parser a r -> Parser a r -> Parser a r
 (p1 <|> p2) s = p1 s ++ p2 s
 
+(<~>) :: (Eq a, Eq r) => Parser a r -> Parser a r -> Parser a r
+(p1 <~> p2) s = p1 s \\ p2 s  
+
 -- Parser Transformers --
 
 negl :: Eq a => a -> Parser a r -> Parser a r
@@ -110,6 +157,11 @@ just p xs = [([], r)
 
 (<@) :: Parser a r -> (r -> s) -> Parser a s
 (p <@ f) s = [(xs, f r) | (xs, r) <- p s]
+
+(<?@) :: Parser a [r] -> (v, r -> v) -> Parser a v
+p <?@ (z, f) = p <@ g
+               where g [] = z
+                     g [x] = f x
 
 some :: Parser a r -> DetParser a r
 some p = snd . head . just p
@@ -136,3 +188,27 @@ sequenced = foldr (<:.>) (succeed [])
 
 choice :: [Parser a r] -> Parser a r
 choice = foldr (<|>) none
+
+chainl :: Parser s a -> Parser s (a -> a -> a) -> Parser s a
+chainl p s = p <.> many (s <.> p) <@ uncurry (foldl (flip ap2))
+
+-- chainr' p s = q
+-- where q = p <*> (option (s <*> q) <?@ (id,ap2) )
+-- <@ flip ap
+
+chainr :: Parser s a -> Parser s (a -> a -> a) -> Parser s a
+chainr p s = many (p <.> s) <.> p <@ uncurry (flip (foldr ap1)) 
+
+first :: Parser a b -> Parser a b
+first p xs | null r = []
+           | otherwise = [head r]
+           where r = p xs
+
+greedy :: Parser a b -> Parser a [b]
+greedy = first . many
+
+greedy1 :: Parser a b -> Parser a [b]
+greedy1 = first . many1
+
+compulsion :: Parser a b -> Parser a [b]
+compulsion = first . option
